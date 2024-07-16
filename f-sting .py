7503086d2fea7c86434c9f2af3fb3c31df761c28
codedreammer <nkanal38@gmail.com>
2024-07-16 17:59:18 +0530

    add the update

--- f-sting .py
+++ f-sting .py
@@ -5,3 +5,8 @@ name = "Harry"
 print(letter.format(name,country))
 
 print(f"Hey my name is {name} and I am from {country}")
+
+price = 49.0999
+txt = f"For only {price:.2f}dollars!"
+print(txt)
+print(type(f"{2*30}"))
